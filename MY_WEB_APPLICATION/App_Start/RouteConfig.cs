9bd9c19c7c5016ac944687dc73646efad66c53af
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-03-03 17:34:35 +0330

    Add project files.

---
+++ MY_WEB_APPLICATION/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+using System.Web.Mvc;
+//using System.Web.Routing;
+
+namespace MY_WEB_APPLICATION
+{
+	public static class RouteConfig
+	{
+		static RouteConfig()
+		{
+		}
+
+		public static void RegisterRoutes(System.Web.Routing.RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			//routes.MapRoute(
+			//	name: "Default",
+			//	url: "{controller}/{action}/{id}",
+			//	defaults: new { controller = "Home", action = "Index", id = System.Web.Mvc.UrlParameter.Optional }
+			//);
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = System.Web.Mvc.UrlParameter.Optional },
+				namespaces: new[] { "MY_WEB_APPLICATION.Controllers" }
+			);
+		}
+	}
+}
---
+++ MY_WEB_APPLICATION/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿namespace MY_WEB_APPLICATION.Controllers
+{
+	public partial class HomeController : Infrastructure.BaseController
+	{
+		public HomeController() : base()
+		{
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Index()
+		{
+			return (View());
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult About()
+		{
+			return (View());
+		}
+
+		[System.Web.Mvc.HttpGet]
+		public virtual System.Web.Mvc.ViewResult Contact()
+		{
+			return (View());
+		}
+	}
+}
